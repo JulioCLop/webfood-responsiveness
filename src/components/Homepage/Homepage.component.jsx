00df8afa77ee8caca16aa76dfb7995f08ea9df67
Julio Lopez <joeyclopez88@gmail.com>
2021-12-11 23:19:44 -0700

    added menu button for small and xs screens

--- src/components/Homepage/Homepage.component.jsx
+++ src/components/Homepage/Homepage.component.jsx
@@ -35,6 +35,8 @@ const Homepage = () => {
                </ul>
             </section>
          </main>
+
+         
       </div>
    );
 };
--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -5,8 +5,9 @@ import './Header.styles.css';
 
 const Header = () => {
     return (
+        <>
         <header id='main-header'>
-            <a  href="#" id='logo'>WebFood</a>
+            <a href="#" id='logo'>WebFood</a>
             <nav>
                 <ul>
                     <li>
@@ -17,8 +18,33 @@ const Header = () => {
                     </li>
                 </ul>
             </nav>
-        </header>
+            <a href="#side-drawer" className='menu-btn'>
+                <span ></span>
+                <span ></span>
+                <span ></span>
+            </a>
+            </header>
+            <aside id='side-drawer'>
+                <header>
+                <a href="#" className='menu-btn'>
+                <span ></span>
+                <span ></span>
+                <span ></span>
+            </a>
+                </header>
+            <nav>
+            <ul>
+                <li>
+                    <a href="">Browse Meals</a>
+                </li>
+                <li>
+                    <a href="#">My Orders</a>
+                </li>
+            </ul>
+        </nav>
+            </aside>
+            </>
     )
-}
+};
 
 export default Header;
